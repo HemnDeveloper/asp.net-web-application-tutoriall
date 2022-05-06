d3e1b9e8098946ee170471eef05dd280b76040ea
HemnDeveloper <105059755+HemnDeveloper@users.noreply.github.com>
2022-05-06 17:06:04 +0300

    Add project files.

---
+++ asp.net web application tutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_web_application_tutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
